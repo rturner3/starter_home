ce76cbd6b9c6ebc99966034698888c7a5facc172
Ryan Turner <turner@uber.com>
2023-01-04 21:01:33 -0800

    First commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,143 @@
+require('plugins')
+if vim.env.VIM_PATH ~= nil and vim.env.VIM_PATH ~= "" then
+    vim.env.PATH = vim.env.VIM_PATH
+end
+
+local opts = { noremap=true, silent=true }
+
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+end
+
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+require('lspconfig').gopls.setup {
+        cmd = {'gopls', '-remote=auto'},
+        on_attach = on_attach,
+        flags = {
+            -- Don't spam LSP with changes. Wait a second between each.
+            debounce_text_changes = 1000,
+        },
+	capabilities = lsp_capabilities,
+	init_options = {
+	    gofumpt = true,
+	    staticcheck = true,
+	},
+}
+
+function FormatAndImports(wait_ms)
+    wait_ms = wait_ms or 3000
+    vim.lsp.buf.format({
+        timeout_ms = wait_ms,
+    })
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+    for _, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+            if r.edit then
+                vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+            else
+                vim.lsp.buf.execute_command(r.command)
+            end
+        end
+    end
+end
+
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+	pattern = {"*.go", "*.lua"},
+	callback = function() FormatAndImports() end,
+})
+
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.o.number = true
+vim.o.laststatus = 2
+
+vim.g.UltiSnipsExpandTrigger = "<tab>"
+vim.g.UltiSnipsJumpForwardTrigger  ="<c-b>"
+vim.g.UltiSnipsJumpBackwardTrigger = "c-z>"
+vim.g.UltiSnipsEditSplit = "vertical"
+vim.g.python3_host_prog = "/opt/homebrew/bin/python3"
+
+-- Set up nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["UltiSnips#Anon"](args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'ultisnips' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+require('lualine').setup {
+  options = {
+    theme = 'powerline',
+  },
+}
+
+-- Treesitter syntax highlighting looks horrible right now
+-- Try this out again later when things are more stable
+--  require'nvim-treesitter.configs'.setup{
+--    ensure_installed = { "go", "lua" },
+--    highlight = {
+--      enable = true,
+--    },
+--  }
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require"fidget".setup{}
